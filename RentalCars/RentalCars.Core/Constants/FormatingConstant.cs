af342775619de5c1591ee2e77b6beffd7cb94bdd
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-10-05 11:03:00 +0300

    Renting cars project is added

---
+++ RentalCars/RentalCars.Core/Constants/FormatingConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalCars.Core.Constants
+{
+    public class FormatingConstant
+    {
+        public const string NormalDateFormat = "dd.mm.yyyy";
+    }
+}
---
+++ RentalCars/RentalCars/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentalCars.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
