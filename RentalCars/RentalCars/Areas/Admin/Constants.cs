91158363690d4fb272cffa3957ebd8e74f09681a
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-10-23 17:36:26 +0300

    admin area added

---
+++ RentalCars/RentalCars/Areas/Admin/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace RentalCars.Areas.Admin
+{
+    public class Constants
+    {
+        public const string AreaName = "Admin";
+    }
+}
--- RentalCars/RentalCars/Program.cs
+++ RentalCars/RentalCars/Program.cs
@@ -65,7 +65,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
