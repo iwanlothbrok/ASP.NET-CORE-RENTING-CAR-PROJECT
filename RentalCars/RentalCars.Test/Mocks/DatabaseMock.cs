eadb78d3d2d14988a9ab74571308ccab1bd5e351
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-11-18 17:45:00 +0200

    tests added

---
+++ RentalCars/RentalCars.Test/Mocks/DatabaseMock.cs
@@ -0,0 +1,20 @@
+﻿namespace RentalCars.Test.Mocks
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using RentalCars.Data;
+    public static class DatabaseMock
+    {
+        public static ApplicationDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+
+                return new ApplicationDbContext(dbContextOptions);
+            }
+        }
+    }
+}
